8ca15e262727a51f9d51a36d0451dac9c69f7a4f
Adam <46858866+weenie-walker-games@users.noreply.github.com>
2019-03-18 23:55:20 -0400

    feat: Create Clear Button Functionality

---
+++ notepad/Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonControl : MonoBehaviour
+{
+
+    public GameObject theText;
+
+    public void ClearText()
+    {
+        theText.GetComponent<InputField>().text = "";
+    }
+
+
+}
